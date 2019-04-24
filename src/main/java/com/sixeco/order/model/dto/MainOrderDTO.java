5ff163a8957ec5c2a7b2ccf5e2836af850c9926f
zhanghe <zhanghe>
2019-04-24 10:52:57 +0800

    代码优化

--- src/main/java/com/sixeco/order/model/dto/MainOrderDTO.java
+++ src/main/java/com/sixeco/order/model/dto/MainOrderDTO.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import reactor.util.annotation.NonNull;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
